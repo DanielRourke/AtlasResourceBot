465cf727afb0e8bc1a827a8c3ef6027b2a35a187
Daniel <daniel.rourke@aussiebroadband.com.au>
2020-07-07 23:55:56 +1000

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtlasResourceBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
