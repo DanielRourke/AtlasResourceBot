b387a01fee8edbd630e33f5696fc4c9f34658dc2
Daniel <daniel.rourke@aussiebroadband.com.au>
2020-07-20 17:22:26 +1000

    Finished Refactor Entity

--- Models/AtlasLogEntry.cs
+++ Models/AtlasLogEntry.cs
@@ -14,7 +14,7 @@ namespace AtlasResourceBot.Modles
 
 
         public Entity entityA{ get; }
-        public string verb { get; }
+        public string action { get; }
         public Entity entityB { get; }
 
         public char gridReferenceLetter { get; }
@@ -24,8 +24,7 @@ namespace AtlasResourceBot.Modles
         
         public override string ToString() => JsonSerializer.Serialize<AtlasLogEntry>(this);
         //dont know how to make this into simlar to macro in c#
-        static private string rgx(string s) => $@"(?<{s}>([^()' ]*\s)*[^()' ]*)";
-
+        
         public AtlasLogEntry(string entry)
         {
             
@@ -45,139 +44,23 @@ namespace AtlasResourceBot.Modles
                 {
 
                     Console.WriteLine("success");
+                   
                     foundMatch = true;
                     //set Recieced Time to Current time
                     RecivedTime = DateTime.Now;
 
                     //set Type to of log to match dictonary key
-
                     string[] words = pattern.Key.Split(' ');
 
-                    Console.WriteLine(words.Length);
+                    //TODO error checking
+                    entityA = new Entity(result, words[0]);
 
-                    Console.WriteLine("success 1");
-                    verb = words[1];
-                    Console.WriteLine("success 2");
-                    //TODO fix this heaping pile into something more dynamic playerA verb ...list of (entities) .add..
-                    if(words[0].Equals("Item"))
-                    {
-                         Console.WriteLine("success item");
-                        entityA.name = result.Groups["item"].Value;
-                        entityA.type = EntityType.Item;
-                    }
-                    else if (words[0].Equals("Ship"))
-                    {
-                        Console.WriteLine("success sss");
-                        entityA.name = result.Groups["ship"].Value;
-                        entityA.type = EntityType.Ship;
-                    }
-                    else if (words[0].Equals("Bed"))
-                    {
-                        Console.WriteLine("success bb");
-                        entityA.name = result.Groups["bed"].Value;
-                        entityA.type = EntityType.Ship;
-                    }
-                    else if (words[0].Equals("Company"))
-                    {
-                        Console.WriteLine("success item");
-                        entityA.name = result.Groups["company"].Value;
-                        entityA.type = EntityType.Company;
-                    }
-                    else if (words[0].Equals("Player"))
-                    {
-                        Console.WriteLine("success pppp");
-                        entityA.name = result.Groups["victim"].Value;
-                        entityA.type = EntityType.Player;
-                        entityA.level =  int.Parse(result.Groups["victimlevel"].Value);
-                        entityA.rank = result.Groups["victimrank"].Value;
-                        entityA.company = result.Groups["victimcompany"].Value;
-                    }
-                    else if (words[0].Equals("Crewmember"))
-                    {
+                    action = words[1];
 
-                        
+                    //add entity if exists
+                    entityB = words.Length > 2 ? new Entity(result, words[2]) : null;
 
-                        Console.WriteLine("success ccccc");
-                        entityA = new Entity();
-                        //entityA.type = EntityType.Crewmember;
-                        Console.WriteLine(result.Groups.Count);
-
-                        for (int i = 0; i < result.Groups.Count; i++)
-                        {
-                            Console.WriteLine(result.Groups[i]);
-                        }
-
-                        Console.WriteLine("successx");
-                        if(result.Groups.TryGetValue("victim", out var o))
-                        {
-                            Console.WriteLine("Found Value");
-                            entityA.name = o.Value;
-                        }
-                        else
-                        {
-                            Console.WriteLine("could not get value");
-                        }
-
-                       
-                        Console.WriteLine("success cccccsssss");
-
-                        
-                     
-                        entityA.level =  int.Parse(result.Groups["victimlevel"].Value);
-                        Console.WriteLine("success ccccssssssssc");
-                        entityA.rank = result.Groups["victimrank"].Value;
-                        Console.WriteLine("success ccccssssssssssssc");
-                        entityA.company = result.Groups["victimcompany"].Value;
-                    }
-                    else
-                    {
-                        entityA = null;
-                    }
- 
-                    // if(entityA.type.Equals(EntityType.Player) || entityA.type.Equals(EntityType.Crewmember))
-                    // {
-
-                    // }
-                    
-                    Console.WriteLine("success 3");
-
-                    entityB = null;
-
-
-                    if(words.Length > 1)
-                    {
-                        if(words[2].Equals("us"))
-                        {
-                            entityB.name = "Grease Gang";//TODO FIX hardcoding
-                            entityB.type = EntityType.Company;
-                        }
-                        else if (words[2].Equals("new"))
-                        {
-                            entityB.name = result.Groups["new"].Value;
-                            entityB.type = EntityType.Crewmember;
-                        }
-                        else if (words[2].Equals("Player"))
-                        {
-                            entityB.name = result.Groups["attacker"].Value;
-                            entityB.type = EntityType.Player;
-                        }
-                        else if (words[2].Equals("Ship"))
-                        {
-                            entityB.name = result.Groups["ship"].Value;
-                            entityB.type = EntityType.Ship;
-                        }
-
-                            
-                        if(entityB.type.Equals(EntityType.Player) || entityB.type.Equals(EntityType.Crewmember))
-                        {
-                            entityB.level =  int.Parse(result.Groups["attackerlevel"].Value);
-                            //entityB.rank = result.Groups["rank"].Value;
-                            entityB.company = result.Groups["attackercompany"].Value;
-                        }
-                    }
-
-
-
+                 
 
                     //Grab game day
                     //TODO add error reporting
@@ -192,18 +75,13 @@ namespace AtlasResourceBot.Modles
 
                     //Grab game time
                     //TODO add error reporting
-                    if (result.Groups["hour"].Success && result.Groups["minute"].Success &&
-                            result.Groups["second"].Success)
-                    {
-                        string time = 
-                            result.Groups["hour"].Value + 
-                            result.Groups["minute"].Value +
-                            result.Groups["second"].Value;
+                    if (result.Groups.TryGetValue("time", out var outTime))
+                    {   
 
-                        if(DateTime.TryParseExact(time, "HH:mm:ss",CultureInfo.InvariantCulture,
-                                DateTimeStyles.NoCurrentDateDefault, out DateTime datetime))
+                        if(DateTime.TryParseExact(outTime.Value, "HH:mm:ss",CultureInfo.InvariantCulture,
+                                DateTimeStyles.NoCurrentDateDefault, out DateTime outDatetime))
                         {
-                            GameTime = datetime;
+                            GameTime = outDatetime;
                         }
                     }
 
@@ -250,44 +128,43 @@ namespace AtlasResourceBot.Modles
             }
 
         }
+        //Regex Helper function
+        static private string rgx(string s) => $@"(?<{s}>([^()' ]*\s)*[^()' ]*)";
 
         private Dictionary<string, Regex> createPatterns()
         {
            Dictionary<string, Regex> patterns = new Dictionary<string, Regex>();
 
-            const string Time = @"Day (?<day>\d{1,4}), (?<time>\d{2}:\d{2}:\d{2}):";
-            const string Grid = @"(?<grid>[A-Z]\d)";
-            const string Coords = @"\[Long: (?<long>(-|)\d*\.\d*) / Lat: (?<lat>(-|)\d*\.\d*)\]";
-            const string Detail = @"(?<detail>\(([^()' ]*)*(\s[^()' ]*)*\))*";
-            const string LevelA = @"Lvl (?<victimlvl>\d{1,3})" ;
-            const string LevelB = @"Lvl (?<attackerlvl>\d{1,3})" ;
+            const string time = @"Day (?<day>\d{1,4}), (?<time>\d{2}:\d{2}:\d{2}):";
+            const string grid = @"(?<grid>[A-Z]\d)";
+            const string coords = @"\[Long: (?<long>(-|)\d*\.\d*) / Lat: (?<lat>(-|)\d*\.\d*)\]";
+            const string detail = @"(?<detail>\(([^()' ]*)*(\s[^()' ]*)*\))*";
+            const string levelA = @"Lvl (?<victimlvl>\d{1,3})" ;
+            const string levelB = @"Lvl (?<attackerlvl>\d{1,3})" ;
 
-            Console.WriteLine("3");
-             Console.WriteLine($@"^{Time} Your '{rgx("ItemA") + Detail}' was destroyed!$");  
-            patterns.Add("ItemA destroyed us",
-                new Regex($@"^{Time} Your '{rgx("ItemA") + Detail}' was destroyed!$"));  
-                Console.WriteLine("4");     
-            patterns.Add("Item auto-decay us",
-                new Regex(pattern: $@"{Time} Your '{rgx("ItemA")+Detail}' was auto-decay destroyed at {Grid} {Coords}"));
-            patterns.Add("Player Killed us",
-                new Regex(pattern: $@"{Time} Your Company killed {rgx("PlayerA")} - {LevelA} \({rgx("victimcompany")} - {rgx("victimrank")}\)!"));
-            patterns.Add("Crewmember Killed us",
-                new Regex(pattern: $@"{Time} Your Company killed {rgx("CrewmemberA")} - {LevelA} \(Crewmember\) \({rgx("company")}\)!"));
+            patterns.Add("ItemA destroyed",
+                new Regex($@"^{time} Your '{rgx("ItemA") + detail}' was destroyed!$"));   
+            patterns.Add("ItemA auto-decay",
+                new Regex(pattern: $@"{time} Your '{rgx("ItemA")+detail}' was auto-decay destroyed at {grid} {coords}"));
+            patterns.Add("PlayerA Killed",
+                new Regex(pattern: $@"{time} Your Company killed {rgx("PlayerA")} - {levelA} \({rgx("CompanyA")} - {rgx("RankA")}\)!"));
+            patterns.Add("CrewmemberA Killed",
+                new Regex(pattern: $@"{time} Your Company killed {rgx("CrewmemberA")} - {levelA} \(Crewmember\) \({rgx("CompanyA")}\)!"));
                 //TODO killed Rodrigo Bill - Lvl 28 (Shoreline Mafia)!
-            patterns.Add("Crewmember Retired us",
-                new Regex(pattern: $@"{Time} Crew member {rgx("CrewmemberA")} - {LevelA} was killed!"));
-            patterns.Add("Crewmember Killed Player",
-                new Regex(pattern: $@"{Time} Crew member {rgx("CrewmemberA")} - {LevelA} was killed by {rgx("PlayerB")} - {LevelB} \({rgx("companyB")}\)!"));
-            patterns.Add("Bed Removed us",
-                new Regex(pattern: $@"{Time} Bed {rgx("BedA")} was removed from the Company!"));
-            patterns.Add("Bed RenamedTo New",
-                new Regex(pattern: $@"{Time} Bed {rgx("BedA")} was renamed to {rgx("ShipB")}!"));
-            patterns.Add("Ship RenamedTo New",
-                new Regex(pattern: $@"{Time} Ship {rgx("ShipA")} was renamed to {rgx("ShipB")}!"));
-            patterns.Add("Ship beingStolen Company", //TODO sort that out
-                new Regex(pattern: $@"{Time} {rgx("CompanyB")} is stealing your {rgx("ShipA")}! ({Grid})"));
-            patterns.Add("Ship Interrupted",
-                new Regex(pattern: $@"{Time} Your claim of {rgx("ShipA")} has been interrupted! ({Grid})"));
+            patterns.Add("CrewmemberA Retired",
+                new Regex(pattern: $@"{time} Crew member {rgx("CrewmemberA")} - {levelA} was killed!"));
+            patterns.Add("CrewmemberA Killed Player",
+                new Regex(pattern: $@"{time} Crew member {rgx("CrewmemberA")} - {levelA} was killed by {rgx("PlayerB")} - {levelB} \({rgx("CompanyB")}\)!"));
+            patterns.Add("BedA Removed",
+                new Regex(pattern: $@"{time} Bed {rgx("BedA")} was removed from the Company!"));
+            patterns.Add("BedA RenamedTo BedB",
+                new Regex(pattern: $@"{time} Bed {rgx("BedA")} was renamed to {rgx("BedB")}!"));
+            patterns.Add("ShipA RenamedTo ShipB",
+                new Regex(pattern: $@"{time} Ship {rgx("ShipA")} was renamed to {rgx("ShipB")}!"));
+            patterns.Add("ShipA beingStolen CompanyB", //TODO sort that out
+                new Regex(pattern: $@"{time} {rgx("CompanyB")} is stealing your {rgx("ShipA")}! ({grid})"));
+            patterns.Add("ShipA Interrupted",
+                new Regex(pattern: $@"{time} Your claim of {rgx("ShipA")} has been interrupted! ({grid})"));
 
                 //Day 355, 10:46:07: joejoe little brother - Lvl 26 destroyed their 'Ship Resources Box (Locked)  (R A T S)')!
 	//Line 177: Day 351, 18:10:32: Crew member joejoe little brother - Lvl 23 was killed by a Yeti - Lvl 4!
